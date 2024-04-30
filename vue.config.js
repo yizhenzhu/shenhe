cb945b0b8f16f9dd00eed75e778ed08e34a12cef
xiaozhenzhu <783380549@qq.com>
2024-04-30 14:52:43 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,9 @@ module.exports = {
       '': {
         // target:'http://10.8.0.120:8778/', //测试环境地址
         // target:'http://localhost:8778', //线上服务器地址
-        target:'http://10.8.0.120:8803',   //新测试环境
+        // target:'http://10.8.0.120:8803',   //新测试环境
         // target:'https://api.vvhan.com/api',
+        target:'http://10.8.0.249:8804',
         ws: true,
         changeOrigin: true,
         secure: false, // 如果是https接口，需要配置这个参数  https:false
